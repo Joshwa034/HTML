4ad4998dcde5d990ae4b2abd542c0f8919f4921d
joshwa fernandes <joshwafernandes74@gmail.com>
2024-01-11 19:20:42 +0530

    Initial commit

---
+++ ch4/hh.sql
@@ -0,0 +1,7 @@
+create table class
+(
+    name varchar(10),
+    std_id int,
+    primary key(std_id)
+
+);
---
+++ ch4/v.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("helo wol");
+    return 0;
+}
